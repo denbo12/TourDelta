d1622d344a43cac6822471142d31f915c8e943b7
denbo12 <denbiprivate@gmail.com>
2021-08-19 08:56:09 -0700

    second commit

---
+++ app/src/main/java/com/denbofa/tourdeltaone/SiteDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.denbofa.tourdeltaone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiteDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_site_details)
+    }
+}
